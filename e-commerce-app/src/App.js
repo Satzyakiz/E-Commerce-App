7aa3cd573e457d5456f2f0bcdcb07b693d395f17
Aritra Bhattacharjee <whoisaritra@gmail.com>
2020-07-01 15:08:33 +0530

    Initial Commit

---
+++ e-commerce-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Components
+import Home from './containers/Home';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <Home />
+        
+        
+    </div>
+  );
+}
+
+export default App;
